4e9d2e386d8f93f782bd0ce29e8957bd5c6c6f05
LevyvoNet <eladlevy2@gmail.com>
2021-10-12 18:31:24 +0300

    adding ID for challenge

--- tests/benchmark_solvers_on_envs.py
+++ tests/benchmark_solvers_on_envs.py
@@ -127,19 +127,19 @@ lvl_to_solvers = {
     2: [
         long_ma_rtdp_pvi_min_describer,
         long_ma_rtdp_min_dijkstra_describer,
-        # long_id_ma_rtdp_min_dijkstra_describer,
-        # long_id_ma_rtdp_min_pvi_describer,
-        # long_id_ma_rtdp_min_rtdp_dijkstra_describer,
+        long_id_ma_rtdp_min_dijkstra_describer,
+        long_id_ma_rtdp_min_pvi_describer,
+        long_id_ma_rtdp_min_rtdp_dijkstra_describer,
         long_ma_rtdp_min_rtdp_dijkstra_describer,
         long_ma_rtdp_pvi_sum_describer,
         long_ma_rtdp_sum_dijkstra_describer,
         long_ma_rtdp_sum_rtdp_dijkstra_describer,
-        # long_id_rtdp_sum_pvi_describer,
-        # long_id_ma_rtdp_sum_pvi_describer,
+        long_id_rtdp_sum_pvi_describer,
+        long_id_ma_rtdp_sum_pvi_describer,
     ],
     3: [
-        # long_id_ma_rtdp_sum_dijkstra_describer,
-        # long_id_ma_rtdp_sum_rtdp_dijkstra_describer
+        long_id_ma_rtdp_sum_dijkstra_describer,
+        long_id_ma_rtdp_sum_rtdp_dijkstra_describer
     ]
 }
 
